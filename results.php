4fbc0f5b5abe1eee05deb252f632da087e1b3e57
Adam Tremarche <atremarche@gmail.com>
2019-07-27 05:53:42 -0700

    migrated js version of assignment into a lighterweight php app

---
+++ results.php
@@ -0,0 +1,147 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta http-equiv="X-UA-Compatible" content="ie=edge">
+
+  <!-- import bootstrap css -->
+  <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+
+  <title>CSC 642 Summer 2019 Individual Assignment Adam Tremarche</title>
+
+  <style>
+      #map {
+        height: 400px;
+        width: 100%;
+       }
+    </style>
+
+</head>
+
+<body>
+
+  <div class="d-flex justify-content-center" style="background-color:#e18582">
+    <h2>CSC 642 Summer 2019 Individual Assignment Adam Tremarche</h2>
+  </div>
+
+  <!-- basic row/column layout -->
+  <div class="row">
+    <div class="col-sm-2"></div>
+    <div class="col-sm-8">
+
+      <br><h1>Results:</h1><br><br>
+
+      <table class="table">
+        <thead>
+          <tr>
+            <th scope="col">Entry Field:</th>
+            <th scope="col">Results:</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <th scope="row">First Name:</th>
+            <td><?php echo $_POST["firstname"]; ?></td>
+          </tr>
+          <tr>
+            <th scope="row">Last Name:</th>
+            <td><?php echo $_POST["lastname"]; ?></td>
+          </tr>
+          <tr>
+            <th scope="row">Email Address:</th>
+            <td><?php echo $_POST["email1"]; ?></td>
+          </tr>
+          <tr>
+            <th scope="row">Phone Number:</th>
+            <td><?php echo $_POST["phone"]; ?></td>
+          </tr>
+          <tr>
+            <th scope="row">Street Address:</th>
+            <td><? php echo $_POST["stAddress"]; ?></td>
+          </tr>
+          <tr>
+            <th scope="row">City:</th>
+            <td><? php echo $_POST["city"]; ?></td>
+          </tr>
+          <tr>
+            <th scope="row">State:</th>
+            <td><? php echo $_POST["state"]; ?></td>
+          </tr>
+          <tr>
+            <th scope="row">Zip Code:</th>
+            <td><? php echo $_POST["zip"]; ?></td>
+          </tr>
+          <tr>
+            <th scope="row">Education Level:</th>
+            <td><? php echo $_POST["education"]; ?></td>
+          </tr>
+          <tr>
+            <th scope="row">Income Bracket:</th>
+            <td><? php echo $_POST["income"]; ?></td>
+          </tr>
+        </tbody>
+      </table>
+
+      <br><br>
+
+      <div id="map" onload="initMap()"></div>
+
+      <br><br>
+
+    </div>
+    <div class="col-sm-2"></div>
+  </div>
+
+  <script>
+
+function initMap() {
+
+  //default map state
+  var map = new google.maps.Map(document.getElementById('map'), {
+    center: {lat: -34.397, lng: 150.644},
+    zoom: 15
+  });
+
+  //get street address
+  var street = <? php echo $_POST["stAddress"]; ?>;
+  var city = <? php echo $_POST["city"]; ?>;
+  var state = <? php echo $_POST["state"]; ?>;
+  var zip = <? php echo $_POST["zip"]; ?>;
+  var address = street.concat(" ", city, " ", state, " ", zip);
+
+  //initialize geocoder
+  geocoder = new google.maps.Geocoder();
+
+  //geocoder encodes address and updates map
+  geocoder.geocode( { 'address': address}, function(results, status) {
+    if (status == 'OK') {
+      map.setCenter(results[0].geometry.location);
+      var marker = new google.maps.Marker({
+          map: map,
+          position: results[0].geometry.location
+      });
+      infowindow = new google.maps.InfoWindow({
+                content: address
+            });
+    } else {
+      alert('Geocode was not successful for the following reason: ' + status);
+    }
+  });
+}
+
+  </script>
+
+
+  <!-- import google maps -->
+  <script async defer
+    src="https://maps.googleapis.com/maps/api/js?key=AIzaSyDu_D1X0YvfaDFs_Iluc469lNKxNm_rtAk&callback=initMap">
+  </script>
+
+  <!-- import js for bootstrap -->
+  <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
+  <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
+
+</body>
+</html>
