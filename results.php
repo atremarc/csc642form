925746ec392e87d81a923586bfcc78e99ada6233
atremarche <atremarc@mail.sfsu.edu>
2019-07-27 21:45:55 -0700

    attempting map w/ address again

--- results.php
+++ results.php
@@ -90,11 +90,29 @@
       var city = "<?php echo $city ?>";
       var state = "<?php echo $state ?>";
       var zip = "<?php echo $zip ?>";
+      var address = street.concat(" ", city, " ", state, " ", zip);
+
       var map;
+      var geocoder;
+
       function initMap() {
+        geocoder = new google.maps.Geocoder();
+
         map = new google.maps.Map(document.getElementById('map'), {
           center: {lat: -34.397, lng: 150.644},
           zoom: 8
+        });
+
+        geocoder.geocode( { 'address': address}, function(results, status) {
+          if (status == 'OK') {
+            map.setCenter(results[0].geometry.location);
+            var marker = new google.maps.Marker({
+              map: map,
+              position: results[0].geometry.location
+            });
+          } else {
+            alert('Geocode was not successful for the following reason: ' + status);
+          }
         });
       }
     </script>
